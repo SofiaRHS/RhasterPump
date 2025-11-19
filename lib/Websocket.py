0da0ff9b1cc137db5c25816e56920016b1469de1
SAN <kotike3@bk.ru>
2025-11-20 04:24:54 +0500

    Fixed namespace bug

--- lib/Websocket.py
+++ lib/Websocket.py
@@ -1,3 +1,4 @@
+# lib/Websocket.py
 import asyncio, websockets, json
 from data.Storage import price_data
 
@@ -8,14 +9,13 @@ class BinanceWS:
         self.symbols = ["clankerusdt", "resolvusdt", "uselessusdt"]
         self.intervals = ["1m", "5m", "15m", "30m"]
 
-    async def subscribe(self):
+    async def _ws_loop(self):
         streams = [f"{s}@kline_{i}" for s in self.symbols for i in self.intervals]
         url = BINANCE_WS + "/".join(streams)
         async with websockets.connect(url) as ws:
             while True:
                 msg = await ws.recv()
                 data = json.loads(msg)
-                # сохраняем данные в price_data
                 key = f"{data['stream']}"
                 k = data["data"]["k"]
                 price_data[key] = {
@@ -24,3 +24,7 @@ class BinanceWS:
                     "close": float(k["c"]),
                     "volume": float(k["v"])
                 }
+
+def start_ws():
+    ws = BinanceWS()
+    asyncio.run(ws._ws_loop())
